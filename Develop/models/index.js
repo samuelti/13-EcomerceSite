5eeaa8de722ee4a66bcfeca019e62026cdb22e8f
samuelti <samuelti2442@gmail.com>
2021-07-29 19:54:24 -0700

    pushing for tutor

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -7,14 +7,15 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   forignKey: 'category_id'
-})
+});
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id'
-})
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, { through: ProductTag});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: ProductTag});
 
 module.exports = {
   Product,
